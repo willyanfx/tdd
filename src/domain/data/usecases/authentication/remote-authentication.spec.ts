9d5fde4583517961c49fc2adfc3bcd88537e1cb0
willyanfx <willyan.fx@gmail.com>
2022-04-19 20:12:50 -0700

    test: ensure test throw error 401

--- src/domain/data/usecases/authentication/remote-authentication.spec.ts
+++ src/domain/data/usecases/authentication/remote-authentication.spec.ts
@@ -2,6 +2,8 @@ import { HttpPostClientSpy } from '../../test/mock-http-client';
 import { RemoteAuthentication } from './remote-authentication';
 import { randDomainName } from '@ngneat/falso';
 import { mockAuthentication } from '../../../test/mock-auth';
+import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error';
+import { HttpStatusCode } from '../../protocols/http/http-response';
 
 type SutTypes = {
   sut: RemoteAuthentication;
@@ -30,5 +32,13 @@ describe('RemoteAuthetication', () => {
     const authParams = mockAuthentication();
     await sut.auth(authParams);
     expect(httpPostClientSpy.body).toEqual(authParams);
+  });
+  test('should throw InvalidCredentialsError if HttpClients return 401', async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.unauthorized
+    };
+    const promise = sut.auth(mockAuthentication());
+    await expect(promise).rejects.toThrow(new InvalidCredentialsError());
   });
 });

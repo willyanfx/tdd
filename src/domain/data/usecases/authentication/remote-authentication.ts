6850ac0310d35e1182bc5b5916b982b05798f34c
willyanfx <willyan.fx@gmail.com>
2022-04-19 20:34:57 -0700

    feat:  error handles

--- src/domain/data/usecases/authentication/remote-authentication.ts
+++ src/domain/data/usecases/authentication/remote-authentication.ts
@@ -1,4 +1,7 @@
+import { BadRequestError } from '@/domain/errors/bad-request-error ';
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error';
+import { NotFoundError } from '@/domain/errors/not-found';
+import { ServerError } from '@/domain/errors/server-error';
 import { AuthenticationParams } from '@/domain/usecases/authentication';
 import { HttpPostClient } from '../../protocols/http/http-post-client';
 import { HttpStatusCode } from '../../protocols/http/http-response';
@@ -16,13 +19,20 @@ export class RemoteAuthentication {
     });
 
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.unauthorized:
-        throw new InvalidCredentialsError();
+      case HttpStatusCode.ok:
         break;
 
+      case HttpStatusCode.unauthorized:
+        throw new InvalidCredentialsError();
+
+      case HttpStatusCode.serverError:
+        throw new ServerError();
+
+      case HttpStatusCode.notFound:
+        throw new NotFoundError();
+
       default:
-        return Promise.resolve();
-        break;
+        throw new BadRequestError();
     }
   }
 }

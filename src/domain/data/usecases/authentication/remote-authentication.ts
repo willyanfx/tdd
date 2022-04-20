71d07b5de6fbd61ee3b95a23472814d07d8b5af3
willyanfx <willyan.fx@gmail.com>
2022-04-19 20:12:08 -0700

    feat: ensure remote auth

--- src/domain/data/usecases/authentication/remote-authentication.ts
+++ src/domain/data/usecases/authentication/remote-authentication.ts
@@ -1,5 +1,7 @@
+import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error';
 import { AuthenticationParams } from '@/domain/usecases/authentication';
 import { HttpPostClient } from '../../protocols/http/http-post-client';
+import { HttpStatusCode } from '../../protocols/http/http-response';
 
 export class RemoteAuthentication {
   constructor(
@@ -8,6 +10,19 @@ export class RemoteAuthentication {
   ) {}
 
   async auth(params: AuthenticationParams): Promise<void> {
-    await this.httpPostClient.post({ url: this.url, body: params });
+    const httpResponse = await this.httpPostClient.post({
+      url: this.url,
+      body: params
+    });
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.unauthorized:
+        throw new InvalidCredentialsError();
+        break;
+
+      default:
+        return Promise.resolve();
+        break;
+    }
   }
 }

82a6e1b9a2e61e9f2434a2f8a07b500636164654
willyanfx <willyan.fx@gmail.com>
2022-04-19 19:42:02 -0700

    chore: config

--- src/domain/test/mock-auth.ts
+++ src/domain/test/mock-auth.ts
@@ -1,4 +1,4 @@
-import { AuthenticationParams } from 'domain/usecases/authentication';
+import { AuthenticationParams } from '@/domain/usecases/authentication';
 import { randEmail, randPassword } from '@ngneat/falso';
 
 export const mockAuthentication = (): AuthenticationParams => ({

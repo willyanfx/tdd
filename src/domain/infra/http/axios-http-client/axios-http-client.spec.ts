615d68e986394ba9d52d8ae62cc8e4fd054ff8c1
willyanfx <willyan.fx@gmail.com>
2022-04-21 20:07:52 -0700

    test: ensure AxiosHttpClient support body

--- src/domain/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/domain/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -16,10 +16,10 @@ const mockPostRequest = (): HttpPostParams<any> => ({
 });
 
 describe('AxiosHttpClient', () => {
-  test('should call axios with correct url and verb', async () => {
+  test('should call axios with correct values', async () => {
     const request = mockPostRequest();
     const sut = makeSut();
     await sut.post(request);
-    expect(mockedAxios.post).toHaveBeenCalledWith(request.url);
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
   });
 });

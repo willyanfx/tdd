062991dbd9721084481e466b4f1f5815bc79cff4
willyanfx <willyan.fx@gmail.com>
2022-05-19 23:14:30 -0700

    test: mock provide for input

--- src/ui/pages/login/login.spec.tsx
+++ src/ui/pages/login/login.spec.tsx
@@ -1,7 +1,9 @@
-import { SubmitButton } from '@/ui/components';
-import { getByTestId, render } from '@testing-library/react';
 import React from 'react';
+import { getByTestId, render } from '@testing-library/react';
+import { randEmail } from '@ngneat/falso';
 import { Login } from './login';
+import { Input, SubmitButton } from '@/ui/components';
+import Context from '@/ui/contexts/form/form-context';
 
 describe('Login Component', () => {
   test('should start with initial state', () => {
@@ -9,7 +11,7 @@ describe('Login Component', () => {
     const errorWrap = getByTestId('error-wrap');
     expect(errorWrap.childElementCount).toBe(0);
   });
-  test('', () => {
+  test('should have initial state as disabled', () => {
     const isDisabled = true;
     const text = 'ABC';
     const { getByTestId } = render(
@@ -18,5 +20,19 @@ describe('Login Component', () => {
     const submitButton = getByTestId('submit') as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
     expect(submitButton.textContent).toBe(text);
+  });
+  test('should input require email', () => {
+    const email = randEmail();
+    const title = 'email is required';
+    const providerProps = {
+      hasError: 'required'
+    };
+    const { getByTestId } = render(
+      <Context.Provider value={providerProps}>
+        <Input type="email" />
+      </Context.Provider>
+    );
+    const emailInputStatus = getByTestId('input-status') as HTMLButtonElement;
+    expect(emailInputStatus.title).toBe(title);
   });
 });

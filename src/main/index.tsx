e9f7ff060417c93a2c194415d123c6742517b472
willyanfx <willyan.fx@gmail.com>
2022-05-19 21:55:04 -0700

    feat: updates

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Login } from '@/ui/pages/login/login';
 import '@/ui/styles/global.scss';
 import Router from './router';
 

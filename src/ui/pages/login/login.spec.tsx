a9dbc998e89517e37e5bedee9a672f94f1c67cd5
willyanfx <willyan.fx@gmail.com>
2022-05-19 22:27:55 -0700

    test: submit button

--- src/ui/pages/login/login.spec.tsx
+++ src/ui/pages/login/login.spec.tsx
@@ -1,11 +1,22 @@
+import { SubmitButton } from '@/ui/components';
 import { getByTestId, render } from '@testing-library/react';
 import React from 'react';
 import { Login } from './login';
 
-describe('', () => {
-  test('should not render spinner and error', () => {
+describe('Login Component', () => {
+  test('should start with initial state', () => {
     const { getByTestId } = render(<Login />);
     const errorWrap = getByTestId('error-wrap');
     expect(errorWrap.childElementCount).toBe(0);
+  });
+  test('', () => {
+    const isDisabled = true;
+    const text = 'ABC';
+    const { getByTestId } = render(
+      <SubmitButton isDisabled={isDisabled}>{text}</SubmitButton>
+    );
+    const submitButton = getByTestId('submit') as HTMLButtonElement;
+    expect(submitButton.disabled).toBe(true);
+    expect(submitButton.textContent).toBe(text);
   });
 });
